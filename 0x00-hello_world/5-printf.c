fc56be1238aa21529504072332c90f6484fd4acb
Becca-E <effiongbecca@gmail.com>
2023-02-20 13:55:54 -0800

    print 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: when successful 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the oucome is a piece of art.\n");
+	return (0);
+}
