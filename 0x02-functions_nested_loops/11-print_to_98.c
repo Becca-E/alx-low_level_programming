96d86af39e645f3da23264ab2503182c285ae2d5
Becca-E <effiongbecca@gmail.com>
2023-02-22 05:44:04 -0800

    98print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,39 +8,17 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	while (n < 98)
 	{
-		for (; n <= 98 ; n++)
-
-		{
-			if (n == 98)
-			{
-				printf(" % d\n ", n);
-
-			}
-
-			else
-			{
-				printf(" % d, ", n);
-			}
-		}
+		printf("%i, ", n);
+		n++;
 	}
-	else
+	while (n > 98)
 	{
-		for (; n >= 98 ; n--)
-
-		{
-			if (n == 98)
-
-			{
-				printf(" % d\n", n);
-
-			}
-			else
-
-			{
-				printf(" % d, ", n);
-			}
-		}
+		printf("%i, ", n);
+		n--;
 	}
+
+	Printf("98");
+	_putchar('\n');
 }
