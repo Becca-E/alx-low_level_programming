afe634131fde0f9f30f67e0e135afe32ade2e6fe
Becca-E <effiongbecca@gmail.com>
2023-02-21 19:44:50 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,11 +5,12 @@
  * @n: the number
  * Return: value of the last digit
  */
-int print_last_difit(int n)
+int print_last_digit(int n)
 {
 	int ld = n % 10;
 
-	if ld *= -1;
+	if (ld < 0)
+		ld *= -1;
 
 	_putchar(ld + '0');
 
