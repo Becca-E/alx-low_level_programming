3bd959e111871cd270e8261045e4666751ee9823
Becca-E <effiongbecca@gmail.com>
2023-03-02 07:39:25 -0800

    03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 	char ch;
 
 	for (ch - 'a'; ch <= 'z'; ch++)
-		putchar(ch);
+		_putchar(ch);
 	for (ch - 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-	putchar('\n');
+		_putchar(ch);
+	_putchar('\n');
 	return (0);
 }
